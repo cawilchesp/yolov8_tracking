d54b58902d6f573b1d647f938adf99797572ddc5
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-05-11 09:15:36 -0500

    Create video_detection.py

---
+++ video_detection.py
@@ -0,0 +1,120 @@
+from ultralytics import YOLO
+import cv2
+import numpy as np
+
+
+data_deque = {}
+frame_deque = {}
+
+
+def compute_color(label: int) -> tuple:
+    """
+    Adds color depending on the class
+    """
+    palette = (2 ** 11 - 1, 2 ** 15 - 1, 2 ** 20 - 1)
+    color_table = {
+        0: (85, 45, 255), # person
+        1: (7, 127, 15),  # bicycle
+        2: (255, 149, 0), # Car
+        3: (0, 204, 255), # Motobike
+        5: (0, 149, 255), # Bus
+        7: (222, 82, 175) # truck
+    }
+
+    if label in color_table.keys():
+        color = color_table[label]
+    else:
+        color = [int((p * (label ** 2 - label + 1)) % 255) for p in palette]
+    
+    return color
+
+
+def draw_boxes(img: np.array, bbox: tuple, class_number: int, confidence: float) -> None:
+    """
+    Draw bounding boxes on frame
+    """
+    x1, y1, x2, y2 = bbox
+    
+    center = (int((x2+x1)/2), int((y2+y1)/2))
+
+    class_filter = [0,1,2,3,5,7]
+    if cls in class_filter:
+        color = compute_color(class_number)
+
+        # Draw boxes
+        cv2.rectangle(img, (x1, y1), (x2, y2), color, 1, cv2.LINE_AA)
+
+        # Draw centers
+        cv2.circle(img, center, 2, color, 2, cv2.LINE_AA)
+
+        # Draw labels
+        label = f'{class_names[class_number]} {confidence:.2f}'
+        t_size = cv2.getTextSize(label, 0, 1/3, 1)[0]
+        cv2.rectangle(img, (x1, y1-t_size[1]-3), (x1 + t_size[0], y1+3), color, -1, cv2.LINE_AA)
+        cv2.putText(img, label, (x1, y1 - 2), 0, 1/3, [225, 255, 255], 1, cv2.LINE_AA)
+
+
+
+
+# class names
+class_names = [ 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 
+             'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+             'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
+             'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard',
+             'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+             'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
+             'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+             'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear',
+             'hair drier', 'toothbrush' ]
+
+
+model = YOLO('../weights/yolov8x.pt')
+
+source_folder = 'C:/Users/cawil/OneDrive/Escritorio/DEEP_CCTV/'
+source_file = '20230412_CCTV_Barrio_Triste'
+cap = cv2.VideoCapture(f'{source_folder}{source_file}.mp4')
+
+
+output_folder = f'{source_folder}{source_file}/'
+
+
+
+# fourcc = 'mp4v'  # output video codec
+# fps = cap.get(cv2.CAP_PROP_FPS)
+# w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+# h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+# vid_writer = cv2.VideoWriter(f'{source_folder}output_{source_file}mp4', cv2.VideoWriter_fourcc(*fourcc), fps, (w, h))
+
+frame = 0
+while True:
+    success, img = cap.read()
+
+    results = model(img, stream=True)
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+            # Bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            bbox = (int(x1), int(y1), int(x2), int(y2))
+
+            # Confidence
+            conf = box.conf[0]
+
+            # Class name
+            cls = int(box.cls[0])
+
+            draw_boxes(img, bbox, cls, conf)
+
+            # vid_writer.write(img)
+    output_name = f'{output_folder}image_{str(frame).zfill(6)}.png'
+    cv2.imwrite(output_name, img)
+    frame += 1
+
+            
+
+    cv2.imshow('source', img)
+    
+    if cv2.waitKey(1) & 0xFF == 27:  # Esc to quit
+        break
+
+# vid_writer.release()
