8d2bae4d519788fd1fe28703ca2d36272731cbdd
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-05-24 13:36:54 -0500

    Update video_detection.py

--- video_detection.py
+++ video_detection.py
@@ -13,9 +13,7 @@ from collections import deque
 from icecream import ic
 
 
-palette = (2 ** 11 - 1, 2 ** 15 - 1, 2 ** 20 - 1)
-
-data_deque = {}
+track_deque = {}
 
 # class names
 class_names = [ 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 
@@ -28,20 +26,9 @@ class_names = [ 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'tr
              'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear',
              'hair drier', 'toothbrush' ]
 
-
-def xyxy_to_xywh(xyxy):
-    """" Calculates the relative bounding box from absolute pixel values. """
-    x1, y1, x2, y2 = xyxy
-    bbox_left = min([x1 , x2 ])
-    bbox_top = min([y1 , y2 ])
-    bbox_w = abs(x1 - x2)
-    bbox_h = abs(y1 - y2)
-    x_c = (bbox_left + bbox_w / 2)
-    y_c = (bbox_top + bbox_h / 2)
-    w = bbox_w
-    h = bbox_h
-    return x_c, y_c, w, h
-
+# Class filter
+class_filter = [0,1,2,3,5,7]
+# class_filter = [0]
 
 def compute_color(label: int) -> tuple:
     """
@@ -65,70 +52,68 @@ def compute_color(label: int) -> tuple:
     return color
 
 
-def draw_boxes(image: np.array, bbox: np.array, object_id: np.array, identities: np.array) -> None:  	                                                                                                                                                                                                            
+def draw_boxes(image: np.array, ds_output: np.array) -> None:
     """
     Draw bounding boxes on frame
     """
-    # Remove tracked point from buffer if object is lost
-    for key in list(data_deque):
-        if key not in identities:
-            data_deque.pop(key)
+    for box in enumerate(ds_output):
+        box_xyxy = box[1][0:4]
+        object_id = box[1][-2]
+        class_id = box[1][-1]
+        if class_id in class_filter:
+            # Draw box
+            x1, y1, x2, y2 = [int(j) for j in box_xyxy]
+            color = compute_color(class_id)
+            label = f'{class_names[class_id]} {int(object_id)}'
+            cv2.rectangle(image, (x1, y1), (x2, y2), color, 2, cv2.LINE_AA)
 
-    for i, box in enumerate(bbox):
-        x1, y1, x2, y2 = [int(j) for j in box]
+            # Draw label
+            t_size = cv2.getTextSize(label, 0, 1/3, 1)[0]
+            cv2.rectangle(image, (x1, y1-t_size[1]-3), (x1 + t_size[0], y1+3), color, -1, cv2.LINE_AA)
+            cv2.putText(image, label, (x1, y1 - 2), 0, 1/3, [225, 255, 255], 1, cv2.LINE_AA)
 
-        center = (int((x2+x1)/2), int((y2+y1)/2))
 
-        id_num = int(identities[i]) if identities is not None else 0
-        if id_num not in data_deque:
-            data_deque[id_num] = deque(maxlen=32)
+def draw_trajectories(image: np.array, ds_output: np.array) -> None:
+    """
+    Draw object trajectories on frame
+    """
+    if (len(ds_output) > 0):
+        objects_id = ds_output[:,-2]
+    
+        # Remove tracked point from buffer if object is lost
+        for key in list(track_deque):
+            if key not in objects_id:
+                track_deque.pop(key)
 
-        data_deque[id_num].appendleft(center)
+    for box in enumerate(ds_output):
+        box_xyxy = box[1][0:4]
+        object_id = box[1][-2]
+        class_id = box[1][-1]
+        if class_id in class_filter:
+            # Draw track line
+            x1, y1, x2, y2 = [int(j) for j in box_xyxy]
+            center = (int((x2+x1)/2), int((y2+y1)/2))
+            if int(object_id) not in track_deque:
+                track_deque[int(object_id)] = deque(maxlen=32)
+            track_deque[int(object_id)].appendleft(center)
+            color = compute_color(class_id)
+            for point1, point2 in zip(list(track_deque[int(object_id)]), list(track_deque[int(object_id)])[1:]):
+                cv2.line(image, point1, point2, color, 2, cv2.LINE_AA)
 
-        color = compute_color(object_id[i])
-        label = f'{class_names[object_id[i]]} {id_num}'
 
-        # Draw box
-        cv2.rectangle(image, (x1, y1), (x2, y2), color, 2, cv2.LINE_AA)
-        
-        # Draw trajectory
-        for k in range(1, len(data_deque[id_num])):
-            if data_deque[id_num][k-1] is None or data_deque[id_num][k] is None:
-                continue
+def write_csv(csv_path: str, ds_output: np.array) -> None:
+    """
+    Write object detection results in csv file
+    """
+    # Save results in CSV
+    with open(csv_path, 'a') as f:
+        f.write(f'{frame_num},{id_num},{str(names[classes_id[i]])},{x1},{y1},{x2-x1},{y2-y1},0,\n')
 
-            cv2.line(image, data_deque[id_num][k-1], data_deque[id_num][k], color, 2)
 
-        # Draw labels
-        t_size = cv2.getTextSize(label, 0, 1/3, 1)[0]
-        cv2.rectangle(image, (x1, y1-t_size[1]-3), (x1 + t_size[0], y1+3), color, -1, cv2.LINE_AA)
-        cv2.putText(image, label, (x1, y1 - 2), 0, 1/3, [225, 255, 255], 1, cv2.LINE_AA)
 
-        # # Save results in CSV
-        # with open(csv_path, 'a') as f:
-        #     f.write(f'{frame_num},{id_num},{str(names[object_id[i]])},{x1},{y1},{x2-x1},{y2-y1},0,\n')
 
 
 def main():
-    # Source
-
-    # source_folder = 'D:/SIER/Videos/DEEP_CCTV/'
-    # source_file = '20230412_CCTV_Barrio_Triste'
-
-    source_folder = 'D:/SIER/Videos/Aforo_Bus/'
-    source_file = 'sgtcootransvi.dyndns.org_01_2023051112113649'
-
-
-    cap = cv2.VideoCapture(f'{source_folder}{source_file}.mp4')
-    fourcc = 'mp4v'  # output video codec
-    fps = cap.get(cv2.CAP_PROP_FPS)
-    w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-    # Output
-    output_folder = f'{source_folder}{source_file}/'
-    vid_writer = cv2.VideoWriter(f'{output_folder}output_{source_file}.mp4', cv2.VideoWriter_fourcc(*fourcc), fps, (w, h))
-
-
     # Initialize Deep-SORT
     cfg_deep = get_config()
     cfg_deep.merge_from_file('deep_sort_pytorch/configs/deep_sort.yaml')
@@ -139,56 +124,81 @@ def main():
                         use_cuda=True)
 
     # Initialize YOLOv8 Model
-    model = YOLO('../weights/yolov8m.pt')
+    model_folder = '../weights/'
+    model_file = 'yolov8m'
+    model = YOLO(f'{model_folder}{model_file}.pt')
 
-    # Class filter
-    class_filter = [0,1,2,3,5,7]
+    # Source
+    # source_folder_name = 'D:/SIER/Videos/DEEP_CCTV/'
+    # source_file_name = '20230412_CCTV_Barrio_Triste'
 
-    # Run inference
+    # source_folder_name = 'D:/SIER/Videos/Aforo_Bus/'
+    # source_file_name = 'sgtcootransvi.dyndns.org_01_2023051112113649'
+    # source_file_name = 'sgtcootransvi.dyndns.org_01_20230511120030951'
+    # source_file_name = 'sgtcootransvi.dyndns.org_01_20230511120254332'
+    # source_file_name = 'sgtcootransvi.dyndns.org_01_20230511121459931'
+
+    source_folder_name = 'D:/SIER/Videos/Ruta_Costera/'
+    source_file_name = 'PTZ010'
+    # source_file_name = 'CAR021'
+
+
+    cap = cv2.VideoCapture(f'{source_folder_name}{source_file_name}.avi')
+    if not cap.isOpened():
+        raise RuntimeError('Cannot open source')
+        
+    fourcc = 'mp4v'  # output video codec
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    # Output
+    output_folder_name = f'{source_folder_name}{source_file_name}/'
+    output_file = f'{output_folder_name}output_{source_file_name}_{model_file}'
+    vid_writer = cv2.VideoWriter(f'{output_file}.mp4', cv2.VideoWriter_fourcc(*fourcc), fps, (w, h))
+    csv_writer = f'{output_file}.csv'
+
+    # Run YOLOv8 inference
     frame_number = 0
     while True:
         success, image = cap.read()
+        if not success: break
         
-        # Process detections
+        # Process YOLOv8 detections
         detections = model(image, stream=True)
         
         for r in detections:
+            
             boxes = r.boxes
 
+            # Non-Maximum Suppression
             values, indices = torch.sort(boxes.conf, descending=True)
             sorted_boxes = boxes[indices]
 
             nms_indices = torch.ops.torchvision.nms(sorted_boxes.xyxy, boxes.conf, 0.5)
             nms_boxes = sorted_boxes[nms_indices]
 
-
-            outputs = deepsort.update(nms_boxes.xywh.cpu(), nms_boxes.conf.cpu(), nms_boxes.cls.cpu(), image)
+            # Deep SORT tracking
+            ds_output = deepsort.update(nms_boxes.xywh.cpu(), nms_boxes.conf.cpu(), nms_boxes.cls.cpu(), image)
             
-            if len(outputs) > 0:
-                ic(outputs)
-                # raise SystemExit(0)
-                bbox_xyxy = outputs[:, :4]
-                identities = outputs[:, -2]
-                object_id = outputs[:, -1]
+            draw_boxes(image, ds_output)
+            draw_trajectories(image, ds_output)
 
-                draw_boxes(image, bbox_xyxy, object_id, identities)
-
-                
 
         vid_writer.write(image)
 
-        # output_name = f'{output_folder}image_{str(frame_number).zfill(6)}.png'
+        # output_name = f'{output_folder_name}image_{str(frame_number).zfill(6)}.png'
         # cv2.imwrite(output_name, image)
         # frame_number += 1
 
-                
-
+        # Visualization
         cv2.imshow('source', image)
         
         if cv2.waitKey(1) & 0xFF == 27:  # Esc to quit
             break
 
     vid_writer.release()
+
 
 if __name__ == "__main__":
     with torch.no_grad():
