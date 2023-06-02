89ba9ab8094ad2e1b402336f0f789590ca4ec4e5
Carlos Andrés Wilches Pérez <32644658+cawilchesp@users.noreply.github.com>
2023-06-02 17:04:17 -0500

    Update video_segmentation.py

--- video_segmentation.py
+++ video_segmentation.py
@@ -16,8 +16,7 @@ from icecream import ic
 
 # VARIABLES
 
-# object tracks
-track_deque = {}
+
 
 # class names
 class_names = [ 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 
@@ -35,19 +34,96 @@ class_filter = [0,1,2,3,5,7]
 # class_filter = [0]
 
 
+def draw_boxes(image: np.array, ds_output: np.array) -> None:
+    """
+    Draw bounding boxes on frame
+    """
+    for box in enumerate(ds_output):
+        box_xyxy = box[1][0:4]
+        class_id = box[1][-1]
+        if class_id in class_filter:
+            # Draw box
+            x1, y1, x2, y2 = [int(j) for j in box_xyxy]
+            color = set_color(class_id)
+            cv2.rectangle(image, (x1, y1), (x2, y2), color, 2, cv2.LINE_AA)
 
 
-def draw_masks(masked_image: np.array, boxes: torch.tensor, masks: torch.tensor) -> None:
+def draw_label(image: np.array, ds_output: np.array) -> None:
+    """
+    Draw object label on frame
+    """
+    for box in enumerate(ds_output):
+        box_xyxy = box[1][0:4]
+        object_id = box[1][-2]
+        class_id = box[1][-1]
+        if class_id in class_filter:
+            # Draw box
+            x1, y1, x2, y2 = [int(j) for j in box_xyxy]
+            color = set_color(class_id)
+            label = f'{class_names[class_id]} {int(object_id)}'
+
+            # Draw label
+            t_size = cv2.getTextSize(label, 0, 1/3, 1)[0]
+            cv2.rectangle(image, (x1, y1-t_size[1]-3), (x1 + t_size[0], y1+3), color, -1, cv2.LINE_AA)
+            cv2.putText(image, label, (x1, y1 - 2), 0, 1/3, [225, 255, 255], 1, cv2.LINE_AA)
+
+
+def draw_trajectories(image: np.array, ds_output: np.array) -> None:
+    """
+    Draw object trajectories on frame
+    """
+    if (len(ds_output) > 0):
+        objects_id = ds_output[:,-2]
+    
+        # Remove tracked point from buffer if object is lost
+        for key in list(track_deque):
+            if key not in objects_id:
+                track_deque.pop(key)
+
+    for box in enumerate(ds_output):
+        box_xyxy = box[1][0:4]
+        object_id = box[1][-2]
+        class_id = box[1][-1]
+        if class_id in class_filter:
+            # Draw track line
+            x1, y1, x2, y2 = [int(j) for j in box_xyxy]
+            center = (int((x2+x1)/2), int((y2+y1)/2))
+            if int(object_id) not in track_deque:
+                track_deque[int(object_id)] = deque(maxlen=32)
+            track_deque[int(object_id)].appendleft(center)
+            color = set_color(class_id)
+            for point1, point2 in zip(list(track_deque[int(object_id)]), list(track_deque[int(object_id)])[1:]):
+                cv2.line(image, point1, point2, color, 2, cv2.LINE_AA)
+
+
+def write_csv(csv_path: str, ds_output: np.array, frame_number: int) -> None:
+    """
+    Write object detection results in csv file
+    """
+    for box in enumerate(ds_output):
+        box_xyxy = box[1][0:4]
+        object_id = box[1][-2]
+        class_id = box[1][-1]
+        if class_id in class_filter:
+            x1, y1, x2, y2 = [int(j) for j in box_xyxy]
+
+            # Save results in CSV
+            with open(f'{csv_path}.csv', 'a') as f:
+                f.write(f'{frame_number},{object_id},{class_names[class_id]},{x1},{y1},{x2-x1},{y2-y1}\n')
+
+
+def draw_masks(image: np.array, boxes: torch.tensor, masks: torch.tensor) -> None:
+    """
+    Draw object masks on frame
+    """
     for index, mask in enumerate(masks):
         class_id = int(boxes.cls[index])
 
         if class_id in class_filter:
             object_mask = mask.data.cpu().numpy()[0]
             color = np.array(set_color(class_id), dtype='uint8')
-            color_mask = np.where(object_mask[...,None], color, masked_image)
-            masked_image = cv2.addWeighted(masked_image, 0.6, color_mask, 0.4, 0)
-
-    return masked_image
+            color_mask = np.where(object_mask[...,None], color, image)
+            cv2.addWeighted(image, 0.6, color_mask, 0.4, 0, image)
         
 
 def main():
@@ -75,7 +151,7 @@ def main():
     # source_file_name = 'sgtcootransvi.dyndns.org_01_20230511120254332'
     # source_file_name = 'sgtcootransvi.dyndns.org_01_20230511121459931'
 
-    source_folder_name = 'D:/SIER/Videos/Ruta_Costera/'
+    source_folder_name = 'D:/INTEIA/Videos/Proyectos/Ruta_Costera/'
     source_file_name = 'PTZ010'
     # source_file_name = 'CAR021'
     # source_file_name = 'OP030'
@@ -116,19 +192,53 @@ def main():
             boxes = r.boxes
             masks = r.masks
 
-            segmented_image = draw_masks(image, boxes, masks)
 
+            # Non-Maximum Suppression
+            values, indices = torch.sort(boxes.conf, descending=True)
+            sorted_boxes = boxes[indices]
+
+            nms_indices = torch.ops.torchvision.nms(sorted_boxes.xyxy, boxes.conf, 0.5)
+            nms_boxes = sorted_boxes[nms_indices]
+
+            # Deep SORT tracking
+            ds_output = deepsort.update(nms_boxes.xywh.cpu(), nms_boxes.conf.cpu(), nms_boxes.cls.cpu(), image)
+            
+
+
+
+            # Visualization
+            if show_boxes: draw_boxes(image, ds_output)
+            if show_labels: draw_label(image, ds_output)
+            if show_tracks: draw_trajectories(image, ds_output)
+            if save_csv: write_csv(output_file_name, ds_output, frame_number)
+            if show_masks: draw_masks(image, boxes, masks)
+
+        vid_writer.write(image)
+
+        # Increase frame number
+        frame_number += 1
+        
         # Visualization
         cv2.imshow('source', image)
-        cv2.imshow('segmented', segmented_image)
 
         if cv2.waitKey(1) & 0xFF == 27:  # Esc to quit
             break
-                
+
+    vid_writer.release()
 
 
 
 if __name__ == "__main__":
+    # object tracks
+    track_deque = {}
+
+    # OPTIONS
+    show_boxes = True
+    show_labels = True
+    show_tracks = True
+    show_masks = True
+    save_csv = True
+    
     with torch.no_grad():
         main()
         
